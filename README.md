e89b03512993ca83ea32f61a334be302b453c9ef
Shruti Mishra <shrutidmishra2002@gmail.com>
2021-12-07 01:28:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+
+## COVID TRACKER APP GUI
+<br>
+<h2> What is python GUI :</h2> 
+<br><br>
+<p>Python offers multiple options for developing GUI (Graphical User Interface). ... It is a standard Python interface to the Tk GUI toolkit shipped with Python. Python with tkinter is the fastest and easiest way to create the GUI applications. Creating a GUI using tkinter is an easy task.
+</p> 
+<br><br>
+
+<h2> About this project : </h2>
+<br><br>
+<p>
+Covid Tracker App GUI by fetching api and using Tkinter module in python.
+</p>
+<br><br>
+
+## 📌 Tech Stack:
+<br><br>
+[![PYTHON](https://img.shields.io/badge/python5%20-%23E34F26.svg?&style=for-the-badge&logo=python5&logoColor=white)]
+[![Tkinter](https://img.shields.io/badge/tkinter3%20-%231572B6.svg?&style=for-the-badge&logo=tkinter3&logoColor=white)]
+<br><br>
+
+## 📌 Main Page:
+<br><br>
+<h2>BEFORE CLICKING ON LOAD</h2>
+<br><br>
+
+<img src="main1.png" alt="output" width="700px" height="400px">
+<br><br>
+<h2>AFTER CLICKING ON LOAD</h2>
+<br><br>
+
+<img src="main2.png" alt="output" width="700px" height="400px">
+<br><br>
+
+
+<h2>📌 Contact :</h2>
+<br><br>
+
+<div align="center">
+
+<a  href="https://www.linkedin.com/in/shruti-mishra-b270a7203/" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin%20-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a><span>
+<a href="mailto:shrutidmishra2002@gmail.com"><img  alt="Gmail" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"/></span>
+
+</div>
+<br>
+<div align="center">
+© 2021 Shruti Mishra </div>
---
+++ corona.py
@@ -0,0 +1,48 @@
+import json
+from tkinter import*
+import requests
+from datetime import*
+
+def getCovidData():
+    api="https://disease.sh/v3/covid-19/all"
+    json_data= requests.get(api).json()
+    total_cases = str(json_data ['cases'])
+    total_deaths = str(json_data ['deaths'])
+    today_cases = str(json_data ['todayCases'])
+    today_deaths = str(json_data ['todayDeaths'])
+    today_recovered = str(json_data ['todayRecovered'])
+    updated_at = json_data ['updated']
+    date = datetime.fromtimestamp(updated_at/1e3)
+    label1.config(text="Total Cases:"+total_cases+"\n"+"Total Deaths:"+total_deaths+"\n"+"Todaycases:"+today_cases+"\n"+"Today Death:"
+    +today_deaths+"\n"+"Today Recovered:"+today_recovered+"\n")
+    label2.config(text=date)
+
+
+    #control backlslash
+    # d=json_data.json()
+    # print(d)
+root= Tk()
+
+# root.geometry("1200x1000")
+root.geometry("600x800")
+# widthxheight
+root.maxsize(600,800)
+root.title("Covid Tracker App")
+f= ("poppins", 15,"bold")
+# to give background colour we use root.config(bg='colour')
+# we can give colour to background by giving bg colour for labels , labels have individual colour
+top=Label(root,text="****COVID TRACKER APP****",font=f,bg='green',fg='black')
+top.pack(pady=10)
+
+button=Button(root, font= f, text="load", command=getCovidData,padx=48)
+button.pack(pady=10)
+label1=Label(root, font=f,bg='green',fg='red')
+label1.pack(pady=10)
+label2=Label(root, font=f,bg='green',fg='black')
+label2.pack()
+photo = PhotoImage(file="mask.png")
+photo_label = Label(image=photo , height=500 , width=500 ,bg='green')
+photo_label.pack()
+root.configure(bg='green')
+
+root.mainloop()
